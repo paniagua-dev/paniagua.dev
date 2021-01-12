e9980996a827663a1b1c749dacdcfc5395694ecf
julio <julio@paniagua.dev>
2021-01-12 22:10:26 +0100

    Add primeng and add tooltip component in icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
-import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {OxumCirclesModule} from 'oxum-circles';
 import {OxumWallpaperModule} from 'oxum-wallpaper';
+import {TooltipModule} from 'primeng/tooltip';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {OxumCirclesModule} from 'oxum-circles';
 import {IconsGridComponent} from './icons-grid/icons-grid.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    IconsGridComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    OxumCirclesModule,
-    OxumWallpaperModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        IconsGridComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        OxumCirclesModule,
+        OxumWallpaperModule,
+        TooltipModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {
 }
