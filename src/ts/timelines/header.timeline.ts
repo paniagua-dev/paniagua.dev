cf194481773aebd20d52720de2be105865b71d50
Paniagua Julio <julio.paniagua@caisse-des-medecins.ch>
2022-01-31 20:57:45 +0100

    Add new animation waves and update style

--- src/ts/timelines/header.timeline.ts
+++ src/ts/timelines/header.timeline.ts
@@ -3,25 +3,46 @@ import {Core} from '../core';
 
 
 const core = new Core();
-export const headerTimeline = () => gsap.timeline()
-    .from('.header .engineer-text', {
+export const headerTimeline = () => {
+
+    gsap.from('.engineer-text', {
         duration: 1,
         opacity: 0,
         delay: 5,
-    })
-    .from('.header .animate-text:nth-child(2)', {
+    });
+    gsap.from('.animate-text:nth-child(2)', {
+        delay: 5,
         duration: 1,
         x: -800,
-    })
-    .from('.header .drop-effect', {
+    });
+    gsap.from('.drop-effect', {
+        delay: 5,
+        opacity: 0,
         scale: 0,
         rotate: 360,
         stagger: 0.1,
-    })
-    .from(['.header .animate-button'], {
+    });
+    gsap.from('.profile__pic--wrapper', {
+        opacity: 0,
+        delay: 7,
+        duration: 10,
+    });
+    gsap.from(['.animate-button'], {
+        delay: 5,
         duration: 1,
         opacity: 0,
         onComplete: () => {
             core.getElement('contact-button').classList.add('active');
         },
     });
+    gsap.from('.waves', {
+        opacity: 0,
+        delay: 3,
+        duration: 20,
+    });
+    gsap.from('.footer p', {
+        opacity: 0,
+        delay: 5,
+        duration: 20,
+    });
+};
